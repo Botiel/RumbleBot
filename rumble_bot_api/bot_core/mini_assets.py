ec5935f1402603ab638bd5580e2391c936839f18
Nuriel <nurielazar@gmail.com>
2023-12-02 18:19:27 +0200

    created data objects for minis assets

---
+++ rumble_bot_api/bot_core/mini_assets.py
@@ -0,0 +1,28 @@
+from rumble_bot_api.bot_core.utils.data_objects import create_asset, Asset
+from rumble_bot_api.desktop_automation_tool import Region, Position
+from pydantic import BaseModel
+
+
+class MiniAssets(BaseModel):
+    ssim: float = 0.8
+    region: Region = Region(top_left=Position(x=330, y=830), bottom_right=Position(x=770, y=970))
+
+    # Minis
+    baron_rivendare: Asset = create_asset(name='baron_rivendare', cost=4)
+    bat_rider: Asset = create_asset(name='bat_rider', cost=2)
+    cairne_bloodhoof: Asset = create_asset('cairne_bloodhoof', cost=5)
+    flamewaker: Asset = create_asset('flamewaker', cost=4)
+    ghoul: Asset = create_asset('ghoul', cost=2)
+    gryphon_rider: Asset = create_asset('gryphon_rider', cost=2)
+    harpies: Asset = create_asset('harpies', cost=3)
+    miner: Asset = create_asset('miner', cost=1)
+    necromancer: Asset = create_asset('necromancer', cost=4)
+    pilot: Asset = create_asset('pilot', cost=3)
+    prowler: Asset = create_asset('prowler', cost=3)
+    pyromancer: Asset = create_asset('pyromancer', cost=3)
+    sylvanas_windrunner: Asset = create_asset('sylvanas_windrunner', cost=6)
+    stonehoof_tauren: Asset = create_asset('stonehoof_tauren', cost=4)
+    darkspear_troll: Asset = create_asset('darkspear_troll', cost=3)
+
+
+MINI_ASSETS = MiniAssets()
