919c49c6895e5a3ea16a7eccc22d2cb1ee7e6d5d
Nuriel <nurielazar@gmail.com>
2023-12-05 20:22:58 +0200

    updated test flows

--- test_flows/full_quest_run.py
+++ test_flows/full_quest_run.py
@@ -1,23 +1,26 @@
 from rumble_bot_api.bot_core.handlers.quests_handler import QuestsHandler
 from rumble_bot_api.bot_core.mini_assets import MINI_ASSETS
+from rumble_bot_api.bot_core.utils.data_objects import MatchLineup
 from test_flows.utils import init_processor
 
 
 if __name__ == '__main__':
 
-    lineup = [
-        MINI_ASSETS.prowler.skill_1,
-        MINI_ASSETS.necromancer.skill_1,
-        MINI_ASSETS.gryphon_rider.skill_1,
-        MINI_ASSETS.pilot.no_skill,
-        MINI_ASSETS.harpies.skill_1,
-        MINI_ASSETS.ghoul.skill_1,
-        MINI_ASSETS.baron_rivendare.skill_1
-
-    ]
-    levelup = [MINI_ASSETS.pilot.name]
+    match_lineup = MatchLineup(
+        hero=MINI_ASSETS.baron_rivendare,
+        lineup=[
+            MINI_ASSETS.prowler.skill_1,
+            MINI_ASSETS.necromancer.skill_1,
+            MINI_ASSETS.gryphon_rider.skill_1,
+            MINI_ASSETS.pilot.no_skill,
+            MINI_ASSETS.harpies.skill_1,
+            MINI_ASSETS.ghoul.skill_1,
+            MINI_ASSETS.baron_rivendare.skill_1
+        ],
+        levelup_list=[MINI_ASSETS.pilot]
+    )
 
     p = init_processor()
-    q = QuestsHandler(p, lineup, levelup)
+    q = QuestsHandler(p, match_lineup)
 
     q.main_loop()
--- test_flows/full_quest_run_2.py
+++ test_flows/full_quest_run_2.py
@@ -1,23 +1,29 @@
 from rumble_bot_api.bot_core.handlers.quests_handler import QuestsHandler
+from rumble_bot_api.bot_core.handlers.hero_manager_handler import HeroManagerHandler
 from rumble_bot_api.bot_core.mini_assets import MINI_ASSETS
+from rumble_bot_api.bot_core.utils.data_objects import MatchLineup
 from test_flows.utils import init_processor
 
 
 if __name__ == '__main__':
 
-    lineup = [
-        MINI_ASSETS.stonehoof_tauren.no_skill,
-        MINI_ASSETS.ghoul.no_skill,
-        MINI_ASSETS.necromancer.no_skill,
-        MINI_ASSETS.gryphon_rider.no_skill,
-        MINI_ASSETS.pilot.no_skill,
-        MINI_ASSETS.harpies.no_skill,
-        MINI_ASSETS.baron_rivendare.no_skill
-
-    ]
-    levelup = [MINI_ASSETS.pilot.name]
+    match_lineup = MatchLineup(
+        hero=MINI_ASSETS.baron_rivendare,
+        lineup=[
+            MINI_ASSETS.stonehoof_tauren.no_skill,
+            MINI_ASSETS.ghoul.no_skill,
+            MINI_ASSETS.necromancer.no_skill,
+            MINI_ASSETS.gryphon_rider.no_skill,
+            MINI_ASSETS.pilot.no_skill,
+            MINI_ASSETS.harpies.no_skill,
+            MINI_ASSETS.baron_rivendare.no_skill
+        ],
+        levelup_list=[MINI_ASSETS.pilot]
+    )
 
     p = init_processor()
-    q = QuestsHandler(p, lineup, levelup)
+    m = HeroManagerHandler(p)
+    m.switch_hero(MINI_ASSETS.baron_rivendare.name)
+    q = QuestsHandler(p, match_lineup)
 
     q.main_loop()
