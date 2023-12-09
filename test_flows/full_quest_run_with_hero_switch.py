52cceedaf028599cb84fdd9b698149d46618fac5
Nuriel <nurielazar@gmail.com>
2023-12-09 13:52:32 +0200

    added flow

---
+++ test_flows/full_quest_run_with_hero_switch.py
@@ -0,0 +1,29 @@
+from rumble_bot_api.bot_core.handlers.quests_handler import QuestsHandler
+from rumble_bot_api.bot_core.handlers.hero_manager_handler import HeroManagerHandler
+from rumble_bot_api.bot_core.mini_assets import MINI_ASSETS
+from rumble_bot_api.bot_core.utils.data_objects import MatchLineup
+from test_flows.utils import init_processor
+
+
+if __name__ == '__main__':
+
+    match_lineup = MatchLineup(
+        hero=MINI_ASSETS.baron_rivendare,
+        lineup=[
+            MINI_ASSETS.stonehoof_tauren.skill_0,
+            MINI_ASSETS.ghoul.skill_0,
+            MINI_ASSETS.necromancer.skill_0,
+            MINI_ASSETS.gryphon_rider.skill_0,
+            MINI_ASSETS.pilot.skill_0,
+            MINI_ASSETS.harpies.skill_0,
+            MINI_ASSETS.baron_rivendare.skill_0
+        ],
+        levelup_list=[MINI_ASSETS.pilot]
+    )
+
+    p = init_processor()
+    m = HeroManagerHandler(p)
+    m.switch_hero(MINI_ASSETS.baron_rivendare.name)
+    q = QuestsHandler(p, match_lineup)
+
+    q.main_loop()
